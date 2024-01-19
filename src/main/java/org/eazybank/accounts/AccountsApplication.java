226894826086346e2743c54137ba31e728d13b4f
dennis githinji <wakahiad@gmail.com>
2024-01-19 19:51:07 +0300

    enhancing documentation info

--- src/main/java/org/eazybank/accounts/AccountsApplication.java
+++ src/main/java/org/eazybank/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package org.eazybank.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAuditImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Dennis Githinji",
+                        email = "wakahiad@gmail.com",
+                        url = "https://dennisgithinji.tech"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://dennisgithinji.tech"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API Documentation",
+                url = "https://dennisgithinji.tech"
+        )
+)
 public class AccountsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
