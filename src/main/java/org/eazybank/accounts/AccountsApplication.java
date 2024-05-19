d46582695358e4271676f87030f161ca8d61f8b2
dennis githinji <wakahiad@gmail.com>
2024-05-19 12:15:05 +0300

    connect to eureka discovery server

--- src/main/java/org/eazybank/accounts/AccountsApplication.java
+++ src/main/java/org/eazybank/accounts/AccountsApplication.java
@@ -9,6 +9,7 @@ import org.eazybank.accounts.dto.AccountsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -34,6 +35,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
                 url = "https://dennisgithinji.tech"
         )
 )
+@EnableDiscoveryClient
 public class AccountsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
